1ba006b9b3718e0f63f3656467aec351343f00d0
TyrannoLee <hebbt@naver.com>
2022-12-20 19:42:13 +0900

    HelloBranchChange

--- testGitHub/src/main/java/test/helloWorld.java
+++ testGitHub/src/main/java/test/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2022-12-20 helloWorld");
+		System.out.println("2022-12-20 helloWorld  change1");
 
 	}
 
