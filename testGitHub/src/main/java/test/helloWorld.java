fca7f30a95073ddf867b9d3318fa49172cf4cc1a
TyrannoLee <hebbt@naver.com>
2022-12-20 19:52:10 +0900

    ChangeClassCodeLine

--- testGitHub/src/main/java/test/helloWorld.java
+++ testGitHub/src/main/java/test/helloWorld.java
@@ -4,6 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 10;
 		System.out.println("2022-12-20 helloWorld  change1");
 
 	}
